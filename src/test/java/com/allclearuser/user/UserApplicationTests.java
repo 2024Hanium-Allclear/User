5741ed49c18170d49c1041905e66863d775cb4ff
mashin2002 <mashin0128@gmail.com>
2024-08-27 02:12:40 +0900

    init

---
+++ src/test/java/com/allclearuser/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allclearuser.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
